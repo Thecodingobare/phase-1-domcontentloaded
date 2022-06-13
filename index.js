4747c470a961dc46abc75f4d7c6871c1121d4197
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-06-14 00:05:44 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+  const para = document.querySelector("p");
+  para.textContent = "This is really cool!"
+  console.log(para);
+});
+
